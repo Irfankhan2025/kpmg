f062b038954b277ef1e5e144b4642a2e4da2f761
Irfankhan2025 <irfankhan.k2025@gmail>
2024-12-13 06:53:53 +0000

    added backend s3

--- provider.tf
+++ provider.tf
@@ -5,8 +5,14 @@ terraform {
       version = "~>5.0"
     }
   }
-}
 
-provider "aws" {
-  region     = "us-west-2"
+  provider "aws" {
+    region = "us-west-2"
+  }
+
+  backend "s3" {
+    bucket = "my-terraform-learn-bucket"
+    key    = "security/terraform.tfstate"
+    region = "us-west-2"
+  }
 }
