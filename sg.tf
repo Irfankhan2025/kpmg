81c7bfe0a04bfe3a7288090b014da22ec480bf17
Irfankhan2025 <irfankhan.k2025@gmail.com>
2024-12-16 12:16:50 +0530

    Delete sg.tf

--- sg.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-
-  tags = {
-    Name = "allow_tls" # this is of data type map
-  }
-}
